39d8d29531defe4598e1ba865526e57835f126c1
deivaxxx <134809626+deivaxxx@users.noreply.github.com>
2025-07-14 21:25:32 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,42 @@
+import java.util.Locale
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://central.sonatype.com/repository/maven-snapshots/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Canvas project directory is not a properly cloned Git repository.
+         
+         In order to build Canvas from source you must clone
+         the Canvas repository using Git, not download a code
+         zip from GitHub.
+         
+         Built Canvas jars are available for download at
+         https://canvasmc.io/downloads
+         
+         See https://github.com/CraftCanvasMC/Canvas/blob/HEAD/CONTRIBUTING.md
+         for further information on building and modifying Canvas.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "canvas"
+for (name in listOf("canvas-api", "canvas-server", "canvas-test-plugin")) {
+    val projName = name.lowercase(Locale.ENGLISH)
+    include(projName)
+    findProject(":$projName")!!.projectDir = file(name)
+}
