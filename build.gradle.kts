c06cdda9698c7085251a3c8b6dfb959c7c10f5a2
de1vaxxx <134809626+deivaxxx@users.noreply.github.com>
2025-12-18 11:35:15 +0300

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,60 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import io.papermc.paperweight.tasks.RebuildGitPatches
-import io.papermc.paperweight.tasks.RebuildBaseGitPatches
 
 plugins {
     java
-    id("io.nvfolia.weaver.patcher") version "1.1.1-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.19"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 paperweight {
-    upstreams.register("folia") {
-        repo = github("PaperMC", "Folia")
-        ref = providers.gradleProperty("foliaCommit")
+    upstreams.register("purpur") {
+        repo = github("PurpurMC", "Purpur")
+        ref = providers.gradleProperty("purpurRef")
 
         patchFile {
-            path = "folia-server/build.gradle.kts"
-            outputFile = file("nvfolia-server/build.gradle.kts")
-            patchFile = file("nvfolia-server/build.gradle.kts.patch")
+            path = "purpur-server/build.gradle.kts"
+            outputFile = file("divinemc-server/build.gradle.kts")
+            patchFile = file("divinemc-server/build.gradle.kts.patch")
         }
         patchFile {
-            path = "folia-api/build.gradle.kts"
-            outputFile = file("nvfolia-api/build.gradle.kts")
-            patchFile = file("nvfolia-api/build.gradle.kts.patch")
+            path = "purpur-api/build.gradle.kts"
+            outputFile = file("divinemc-api/build.gradle.kts")
+            patchFile = file("divinemc-api/build.gradle.kts.patch")
         }
         patchRepo("paperApi") {
             upstreamPath = "paper-api"
-            patchesDir = file("nvfolia-api/paper-patches")
+            patchesDir = file("divinemc-api/paper-patches")
             outputDir = file("paper-api")
         }
-        patchDir("foliaApi") {
-            upstreamPath = "folia-api"
+        patchDir("purpurApi") {
+            upstreamPath = "purpur-api"
             excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
-            patchesDir = file("nvfolia-api/folia-patches")
-            outputDir = file("folia-api")
+            patchesDir = file("divinemc-api/purpur-patches")
+            outputDir = file("purpur-api")
         }
+    }
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    tasks.compileJava {
+        options.compilerArgs.add("-Xlint:-deprecation")
+        options.isWarnings = false
+    }
+
+    tasks.withType(JavaCompile::class.java).configureEach {
+        options.isFork = true
+        options.forkOptions.memoryMaximumSize = "4G"
     }
 }
 
@@ -45,125 +64,50 @@ subprojects {
 
     extensions.configure<JavaPluginExtension> {
         toolchain {
-            languageVersion = JavaLanguageVersion.of(22)
+            languageVersion = JavaLanguageVersion.of(21)
         }
     }
 
-    repositories {
-        mavenCentral()
-        maven(paperMavenPublicUrl)
-    }
-
-    tasks.withType<AbstractArchiveTask>().configureEach {
-        isPreserveFileTimestamps = false
-        isReproducibleFileOrder = true
-    }
-    tasks.withType<JavaCompile>().configureEach {
+    tasks.withType<JavaCompile> {
         options.encoding = Charsets.UTF_8.name()
-        options.release = 22
+        options.release = 21
         options.isFork = true
-        options.compilerArgs.addAll(listOf("-Xlint:-deprecation", "-Xlint:-removal"))
     }
-    tasks.withType<Javadoc>().configureEach {
+    tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
     }
-    tasks.withType<ProcessResources>().configureEach {
+    tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
-    tasks.withType<Test>().configureEach {
+    tasks.withType<Test> {
         testLogging {
             showStackTraces = true
             exceptionFormat = TestExceptionFormat.FULL
             events(TestLogEvent.STANDARD_OUT)
         }
     }
-}
 
-project(":nvfolia-api") {
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+        maven("https://jitpack.io")
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+    }
+
     extensions.configure<PublishingExtension> {
         repositories {
-            maven("https://central.sonatype.com/repository/maven-snapshots/") {
-                name = "central"
-	            credentials(PasswordCredentials::class) {
-                    username = System.getenv("PUBLISH_USER")
-                    password = System.getenv("PUBLISH_TOKEN")
-                }
-            }
-        }
+            maven("https://repo.bxteam.org/snapshots") {
+                name = "divinemc"
 
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-
-                afterEvaluate {
-                    pom {
-                        name.set("nvfolia-api")
-                        description.set(this.description)
-                        url.set("https://github.com/Craftnvfolia/nvfolia")
-                        licenses {
-                            license {
-                                name.set("GNU Affero General Public License v3.0")
-                                url.set("https://github.com/Craftnvfolia/nvfolia/blob/HEAD/LICENSE")
-                                distribution.set("repo")
-                            }
-                        }
-                        developers {
-                            developer {
-                                id.set("nvfolia-team")
-                                name.set("nvfolia Team")
-                                organization.set("nvfolia")
-                                organizationUrl.set("https://papermc.io")
-                                roles.add("developer")
-                            }
-                        }
-                        scm {
-                            url.set("https://github.com/Craftnvfolia/nvfolia")
-                        }
-                    }
-                }
+                credentials.username = System.getenv("REPO_USERNAME")
+                credentials.password = System.getenv("REPO_PASSWORD")
             }
         }
     }
 }
 
-// build publication
-tasks.register<Jar>("createMojmapClipboardJar") {
-    dependsOn(":nvfolia-server:createMojmapPaperclipJar")
-}
-
-tasks.register("buildPublisherJar") {
-    dependsOn(":createMojmapClipboardJar")
-
+tasks.register("printMinecraftVersion") {
     doLast {
-        val buildNumber = System.getenv("BUILD_NUMBER") ?: "local"
-
-        val paperclipJarTask = project(":nvfolia-server").tasks.getByName("createMojmapPaperclipJar")
-        val outputJar = paperclipJarTask.outputs.files.singleFile
-        val outputDir = outputJar.parentFile
-
-        if (outputJar.exists()) {
-            val newJarName = "nvfolia-build.$buildNumber.jar"
-            val newJarFile = File(outputDir, newJarName)
-
-            outputDir.listFiles()
-                ?.filter { it.name.startsWith("nvfolia-build.") && it.name.endsWith(".jar") }
-                ?.forEach { it.delete() }
-            outputJar.renameTo(newJarFile)
-            println("Renamed ${outputJar.name} to $newJarName in ${outputDir.absolutePath}")
-        }
-    }
-}
-
-// patching scripts
-tasks.register("fixupMinecraftFilePatches") {
-    dependsOn(":nvfolia-server:fixupMinecraftSourcePatches")
-}
-
-allprojects {
-    tasks.withType<RebuildGitPatches>().configureEach {
-        filterPatches = false
-    }
-    tasks.withType<RebuildBaseGitPatches>().configureEach {
-        filterPatches = false
+        println(providers.gradleProperty("mcVersion").get().trim())
     }
 }
