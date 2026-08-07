a39c7ffa58c8c480cc8bc6703d3eb2e945b59bd7
de1vaxxx <134809626+deivaxxx@users.noreply.github.com>
2026-08-07 15:42:01 +0300

    enable tests build

---
+++ nvfolia-api/paper-patches/files/src/java/org/bukkit/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Integration with Imanity Software's Raytrace AntiXray for better use of this plugin
+ * <p>
+ * Original project: <a href="https://github.com/Imanity-Software/raytrace-antixray-spigot-sdk">GitHub Repository</a>
+ */
+@NullMarked
+package dev.imanity.antixray.sdk;
+
+import org.jspecify.annotations.NullMarked;
